2c2c64ba6141f803f9d08e5872d706defc9bc6ef
danyloday <danil.vladyka1@gmail.com>
2024-08-18 19:25:58 +0300

    Test push

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
 
     implementation(libs.androidx.lifecycle.runtime.ktx)
+
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
